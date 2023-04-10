4aca506cc872bd1ec6a9bb65c35a68664854a77a
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-04-10 15:21:52 -0400

    Created controller to send words to a game id and necessary models

---
+++ app/Http/Controllers/SendWordController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Word;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SendWordController extends Controller
+{
+    public function send(Request $request){
+        $request->validate([
+            'word' => 'required|min:4|max:255',
+        ]);
+        $exist = Word::where('username', Auth::user()->username)->where('word', $request->word)->doesntExist();
+        if($exist){
+            $word = new Word;
+            $word->game_id = $request->game_id;
+            $word->username = Auth::user()->username;
+            $word->pathImage = $request->pathImage;
+            $word->word = $request->word;
+            $word->save();
+            return $request;
+        }else{
+            return abort(422, 'The word was already sended') ;
+        }
+    }
+}
