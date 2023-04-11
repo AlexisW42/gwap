1af91b3b4b043df0647174b8e72b06c56f4e96fc
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-04-11 00:06:20 -0400

    Deleted innecessary methods

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -19,33 +19,4 @@ class ImageController extends Controller
         $image->save();
         return dump($request);
     }
-
-    public function getImages()
-    {
-        $images = Image::get();
-
-        if (sizeof($images) >= 3) {
-            $rand_keys = $this->numbersWithoutRep(sizeof($images)-1);
-            return view('game', ['images' => [$images[$rand_keys[0]], $images[$rand_keys[1]], $images[$rand_keys[2]]]]);
-        }
-
-        return 'There are not enough images in server to play a game';
-    }
-
-    private function numbersWithoutRep($max): array
-    {
-        $numbers = [rand(0, $max)];
-        while (sizeof($numbers) < 3) {
-            $value = rand(0, $max);
-            $bool = false;
-            foreach ($numbers as $number) {
-                if($number== $value)
-                    $bool = true;
-            }
-            if(!$bool){
-                array_push($numbers, $value);
-            }
-        }
-        return $numbers;
-    }
 }
