af65036aef61c1a9648791b999cda5bbd0bf7b23
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-04-12 22:52:31 -0400

    Player dashboard styled

--- resources/views/player/dashboard.blade.php
+++ resources/views/player/dashboard.blade.php
@@ -7,12 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex flex-col bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in as player! ") }} {{ Auth::user()->username }}
+            <div class="p-6 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 flex flex-col items-center text-gray-900 content-center">
+                    <div class="p-4 text-gray-900 self-start">
+                        {{ __("You're logged in as player! ") }} {{ Auth::user()->username }}
+                    </div>
+                    <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                       href="{{url('/play-game')}}">Create or join to a game</a>
                 </div>
-                <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                   href="{{url('/play-game')}}">Create or join to a game</a>
             </div>
         </div>
     </div>
